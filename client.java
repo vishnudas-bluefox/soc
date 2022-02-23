b02a7348814cd914d6ba4d0f9ab2ea44874de42b
Vishnudas <73219501+vishnudas-bluefox@users.noreply.github.com>
2022-02-23 11:20:37 +0530

    Update client.java

--- client.java
+++ client.java
@@ -1,1 +1,32 @@
 
+import java.net.*;
+
+import java.io.*;
+
+public class client{
+
+public static void main(String arg[]) throws IOException {
+
+Socket s=new Socket("localhost",5000);
+
+PrintWriter out=new PrintWriter(s.getOutputStream(),true);
+
+BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+BufferedReader kln=new BufferedReader(new InputStreamReader(System.in));
+
+while(true)
+
+{
+
+String userInput=kln.readLine();
+
+out.println(userInput);
+
+System.out.println("server says:"+in.readLine());
+
+}
+
+}
+
+}
