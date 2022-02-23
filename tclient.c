3efcc80e501ff338f330c4fc392ade20f2b64281
Vishnudas <73219501+vishnudas-bluefox@users.noreply.github.com>
2022-02-23 11:21:44 +0530

    Update tclient.c

--- tclient.c
+++ tclient.c
@@ -1,1 +1,42 @@
 
+import java.net.*;
+
+import java.io.*;
+
+public class tclient
+
+{
+
+public static void main(String args[]) throws IOException
+
+{
+
+Socket s=new Socket("localhost",2000);
+
+PrintWriter pr=new PrintWriter(s.getOutputStream());
+
+InputStreamReader in=new InputStreamReader(System.in);
+
+BufferedReader  b=new BufferedReader(in);
+
+InputStreamReader ino=new InputStreamReader(s.getInputStream());
+
+BufferedReader bf=new BufferedReader(ino);
+
+while(true){
+
+String stt=b.readLine();
+
+pr.println(stt);
+
+pr.flush();
+
+String str=bf.readLine();
+
+System.out.println("server:"+str);
+
+}
+
+}
+
+}
